c0302bda1824d378a4475dac1553f0ce8cd22c5b
YetronLives <cnnorteyjr@gmail.com>
2025-04-10 17:39:12 -0400

    updated models and homepage

---
+++ dotnetTest/Models/Member.cs
@@ -0,0 +1,6 @@
+namespace dotnetTest.Models;
+
+public class Member
+{
+    
+}
---
+++ dotnetTest/Models/Tasks.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace dotnetTest.Models;
+
+public class Task
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+
+    public string? TaskId { get; set; }
+    
+    public string? ProjectId { get; set; }
+    
+    [Required]
+    public string TaskDescription  { get; set; }
+    
+    [Required]
+    public string TaskDueDate  { get; set; }
+    
+    [Required]
+    public bool TaskActive { get; set; }
+
+}
