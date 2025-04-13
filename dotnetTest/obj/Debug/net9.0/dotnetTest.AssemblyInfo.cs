c6326586db1647034c5a7da785c2e8a5cd8cb545
YetronLives <cnnorteyjr@gmail.com>
2025-04-12 22:22:33 -0400

    added new favicon.ico file

--- dotnetTest/obj/Debug/net9.0/dotnetTest.AssemblyInfo.cs
+++ dotnetTest/obj/Debug/net9.0/dotnetTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb420a92e9276b91e5a970108b1d24fb3502341")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4713be90d44d317e5133b43be165d8641f493dda")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
