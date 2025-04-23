8ca77eddfb2c3499c4a421aa4af05f0bc450f16d
Maan Bhagat <95007423+nuclear218@users.noreply.github.com>
2025-04-23 16:23:09 -0400

    Added css for create project page

--- dotnetTest/obj/Debug/net9.0/dotnetTest.AssemblyInfo.cs
+++ dotnetTest/obj/Debug/net9.0/dotnetTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a13c5948ac7f500447483f2d8c9c79e8b47f7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a1a8f9cca77e6835350dcd0671e42139ec7db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
